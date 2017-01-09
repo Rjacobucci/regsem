395d240c992d715f1a2feb2a1693d0245ebc5424
Ross Jacobucci <rcjacobuc@gmail.com>
2017-01-09 13:33:13 -0800

    max.iter not working in cv_regsem

--- R/coord_desc.R
+++ R/coord_desc.R
@@ -307,7 +307,7 @@ coord_desc <- function(start,func,type,grad,hess,hessFun,pars_pen,model,lambda,m
   #  st.crit2 <- all(abs(gg) < .01)
     dif <- abs(vals[count+1] - vals[count])
     #print(dif)
-
+  print(count)
    # print(rbind(update.pars,t(gg2)))
     #print(round(dif,5))
    # print(as.vector(round(gg,3)))
