5060258426e7fff54ad22853d82cdf8f71be3bfd
Ross Jacobucci <rcjacobuc@gmail.com>
2017-06-07 09:38:16 -0700

    summary.cvregsem working

--- R/summary.cvregsem.R
+++ R/summary.cvregsem.R
@@ -1,23 +1,34 @@
 #'
 #' print information about cvregsem object
-summary.cvregsem <- function(x, ...) 
+#' @param x cv_regsem
+#' @param fit Which fit index to use
+#' @export
+summary.cvregsem <- function(x,fit="BIC",...)
 {
-  lowest.id <- which.min(r1[[2]][,4])
-  lenpar <- length(r1$pars_pen)
+  lowest.id <- which.min(x[[2]][,fit])
+  lenpar <- length(x$pars_pen)
 
-  sm <- list(lowest.id=lowest.id, lenpar=lenpar, min.lambda=min(r1[[2]][,1]),
-             max.lambda=max(r1[[2]][,1]), lowest.lambda=r1[[2]][lowest.id,1])
-  
+  sm <- list(lowest.id=lowest.id, lenpar=lenpar, min.lambda=min(x[[2]][,1]),
+             max.lambda=max(x[[2]][,1]), lowest.lambda=x[[2]][lowest.id,1])
+
   class(sm) <- "summary.cvregsem"
-  return(sm)
+
+
+  print.summary.cvregsem <- function(x, ...)
+  {
+    string <- paste0("CV regsem Object\n",
+                     " Number of parameters regularized: ",x$lenpar,"\n",
+                     " Lambda ranging from ",x$min.lambda," to ",x$max.lambda,"\n",
+                     " Lowest Fit Lambda: ", x$lowest.lambda,"\n\n")
+    cat(string)
+  }
+
+
+
+  print(sm)
+
 }
 
 
-print.summary.cvregsem <- function(x, ...)
-{
-  string <- paste0("CV regsem Object\n",
-                   " Number of parameters regularized: ",x$lenpar,"\n",
-                   " Lambda ranging from ",x$min.lambda," to ",x$max.lambda,"\n",
-                   " Lowest BIC Lambda: ", x$lowest.lambda,"\n\n")
-  cat(string)
-}
+
+
